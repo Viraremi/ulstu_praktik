809d23f0e7c4b9805e59874f31632dcb9db16f95
Viraremi <vlad.zaharov.1718@gmail.com>
2024-11-25 11:27:05 +0400

    1) Реализована автоматическая генерация недостающих папок при сохранении csv файла
    2) Решение проблемы неполных файлов:
    Таблицы, в которых нет хотя бы одной заполненной строки, игнорируются
    
    3) Добавлен список таблиц, которые стоит игнорировать. Сейчас это используется чтобы не обрабатывать два раза одни и те же таблицы

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import os
 
 id_SubFed = ['Республика Башкортостан',
              'Республика Марий Эл',
@@ -19,6 +20,14 @@ id_SubFed = ['Республика Башкортостан',
 ulsk = [
     'Ульяновская область'
 ]
+
+# Создаем нужные папки если их нет
+
+def save_to_csv(dataframe, file_path, separator):
+    folder = os.path.dirname(file_path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    dataframe.to_csv(file_path, sep=separator)
 
 # Читаем настройки форматирования из файла
 
@@ -40,29 +49,34 @@ def full_exel_to_csv(path:str, year: int, settings):
     return df
 
 def full_dpo_gs_to_csv(path: str, year: int, settings):
+    print('\nDPO_GS')
     df1 = full_exel_to_csv(path, year, settings['DPO_GS_1'])
     df2 = full_exel_to_csv(path, year, settings['DPO_GS_2'])
 
     result = pd.concat([df1, df2], axis=0)
-    result.to_csv('CSVs/' + path[:-5] + '/DPO_GS_other.csv')
+    save_to_csv(result, 'CSVs/' + path[:-5] + '/DPO_GS.csv', ';')
 
 def full_dpo_gs_other_to_csv(path: str, year: int, settings):
+    print('\nDPO_GS_other')
     df1 = full_exel_to_csv(path, year, settings['DPO_GS_other_1'])
     df2 = full_exel_to_csv(path, year, settings['DPO_GS_other_2'])
     df3 = full_exel_to_csv(path, year, settings['DPO_GS_other_3'])
 
     result = pd.concat([df1, df2], axis=0)
     result = pd.concat([result, df3], axis=0)
-    result.to_csv('CSVs/' + path[:-5] + '/DPO_GS_other.csv')
+    save_to_csv(result, 'CSVs/' + path[:-5] + '/DPO_GS_other.csv', ';')
 
-# Функции обработки для файлов только с Ульяновской областю
+# Функции обработки для файлов только с Ульяновской областью
 
 def ul_exel_to_csv(path:str, year: int, settings):
     print('Обработка ' + settings['sheet'] + '...')
     df = pd.read_excel('data_xlsx/' + path, sheet_name=settings['sheet'], header=None)
     df = df.iloc[settings['iloc_rows'][0]:settings['iloc_rows'][1], settings['iloc_columns'][0]:settings['iloc_columns'][1]] # Обрезаем лишнее
     df = df.drop(settings['drop_column'], axis=1)
-    df = df.dropna(how='all')
+    df = df.dropna(how='any')
+    if (df.size == 0):
+        print('Ошибка ' + settings['sheet'])
+        return df
     df = df.stack().reset_index(drop=True).to_frame() # Складываем таблицу в одну большую строку
     m_id = pd.MultiIndex.from_product([ulsk] + settings['m_id_lists'], names=settings['m_id_names'])
     df = df.set_axis(m_id, axis=0) # Присваиваем мультииндекс
@@ -71,28 +85,47 @@ def ul_exel_to_csv(path:str, year: int, settings):
     return df
 
 def ul_dpo_gs_to_csv(path: str, year: int, settings):
-    df1 = full_exel_to_csv(path, year, settings['DPO_GS_1'])
-    df2 = full_exel_to_csv(path, year, settings['DPO_GS_2'])
+    print('\nDPO_GS')
+    df1 = ul_exel_to_csv(path, year, settings['DPO_GS_1'])
+    if (df1.size == 0):
+        print('Ошибка DPO_GS_1')
+        return
+    df2 = ul_exel_to_csv(path, year, settings['DPO_GS_2'])
+    if (df2.size == 0):
+        print('Ошибка DPO_GS_2')
+        return
 
     result = pd.concat([df1, df2], axis=0)
-    result.to_csv('CSVs/' + path[:-5] + '/DPO_GS.csv')
+    save_to_csv(result, 'CSVs/' + path[:-5] + '/DPO_GS.csv', ';')
 
 def ul_dpo_gs_other_to_csv(path: str, year: int, settings):
+    print('\nDPO_GS_other')
     df1 = ul_exel_to_csv(path, year, settings['DPO_GS_other_1'])
+    if (df1.size == 0):
+        print('Ошибка DPO_GS_other_1')
+        return
     df2 = ul_exel_to_csv(path, year, settings['DPO_GS_other_2'])
+    if (df2.size == 0):
+        print('Ошибка DPO_GS_other_2')
+        return
     df3 = ul_exel_to_csv(path, year, settings['DPO_GS_other_3'])
+    if (df3.size == 0):
+        print('Ошибка DPO_GS_other_3')
+        return
 
     result = pd.concat([df1, df2], axis=0)
     result = pd.concat([result, df3], axis=0)
-    result.to_csv('CSVs/' + path[:-5] + '/DPO_GS_other.csv')
+    save_to_csv(result, 'CSVs/' + path[:-5] + '/DPO_GS_other.csv', ';')
 
-#Оснвной блок
+#Основной блок
 
-# !!!ПРОБЛЕМА!!!
-# Некоторые таблицы не заполены до конца даже по строкам ульяновской области,
-# из за чего не получается их нормально прочитать
-# 17.1. Профразвитие
-# 17.2. Профразвитие
+ignore_sheet = [
+    'DPO_GS_1',
+    'DPO_GS_2',
+    'DPO_GS_other_1',
+    'DPO_GS_other_2',
+    'DPO_GS_other_3'
+]
 
 #Список и обработка ульяновских файлов
 print('\nОбработка ульяновских файлов\n')
@@ -103,13 +136,15 @@ ul_data_xlsx = {
     '2022_ul.xlsx' : 2022,
     '2023_ul.xlsx' : 2023,
 }
-# for file in ul_data_xlsx.items():
-#     print('\nОткрываем файл ' + file[0])
-#     for data in sheet_settings.items():
-#         df = ul_exel_to_csv(file[0], file[1], data[1])
-#         df.to_csv('CSVs/' + file[0][:-5] + '/' + data[1]['csv_path'], sep=';')
-#     full_dpo_gs_to_csv(file[0], file[1], sheet_settings)
-#     full_dpo_gs_other_to_csv(file[0], file[1], sheet_settings)
+for file in ul_data_xlsx.items():
+    print('\nОткрываем файл ' + file[0])
+    for data in sheet_settings.items():
+        if (data[0] in ignore_sheet): continue
+        df = ul_exel_to_csv(file[0], file[1], data[1])
+        if (df.size != 0):
+            save_to_csv(df, 'CSVs/' + file[0][:-5] + '/' + data[1]['csv_path'], ';')
+    ul_dpo_gs_to_csv(file[0], file[1], sheet_settings)
+    ul_dpo_gs_other_to_csv(file[0], file[1], sheet_settings)
 
 #Список и обработка полноценных файлов
 print('\nОбработка полноценных файлов\n')
@@ -122,8 +157,9 @@ full_data_xlsx = {
 for file in full_data_xlsx.items():
     print('\nОткрываем файл ' + file[0])
     for data in sheet_settings.items():
+        if (data[0] in ignore_sheet): continue
         df = full_exel_to_csv(file[0], file[1], data[1])
-        df.to_csv('CSVs/' + file[0][:-5] + '/' + data[1]['csv_path'], sep=';')
+        save_to_csv(df, 'CSVs/' + file[0][:-5] + '/' + data[1]['csv_path'], ';')
     full_dpo_gs_to_csv(file[0], file[1], sheet_settings)
     full_dpo_gs_other_to_csv(file[0], file[1], sheet_settings)
 
