a402780251c06fcc4330e8cfaadc3c07774c4c9d
Viraremi <74133988+Viraremi@users.noreply.github.com>
2025-03-19 11:11:24 +0400

    Правки

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
-import sheet_settings as stgs
+import sheet_settings as sttgs
 import sheet_formating as frmt
 
 # Читаем настройки форматирования из файла
-stgs.get_settings()
+sttgs.get_settings()
 with open("all_settings.json", "r", encoding="utf-8") as file:
     sheet_settings = json.load(file)
     print("Настройки форматирования получены!")
